fe254ae27a2685f2f8371e620b530b4d4ec5f6e1
Diego <wille.diegoricardo@gmail.com>
2019-04-28 18:33:50 -0300

    Perguntas e respostas validado

--- PPI2/questions_and_answers/urls.py
+++ PPI2/questions_and_answers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.questions_list, name='home'),        
+    path('', views.questions_list, name='home'),
+    path('', views.questions_list, name='questions_list'),     
 ]
