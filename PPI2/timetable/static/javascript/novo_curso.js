12330b013fbbef867ee6ad0099e1219e73ff19c6
Diego <wille.diegoricardo@gmail.com>
2019-05-15 21:01:31 -0300

    Adaptacao das paginas de login utilizando MDL. Ajustes de tamanho e espacamento em botoes.

--- PPI2/timetable/static/javascript/novo_curso.js
+++ PPI2/timetable/static/javascript/novo_curso.js
@@ -1,17 +1,13 @@
-let modal = document.getElementById("novo_curso_modal");
+let modalNovoCurso = document.getElementById("novo_curso_modal");
 let ncbtn = document.getElementById("novo_curso_button");
 let close_button = document.getElementsByClassName("close")[0];
 
 ncbtn.onclick = function() {
-  modal.style.display = "block";
+  modalNovoCurso.style.display = "block";
 }
 
-// close_button.onclick = function () {
-//   modal.style.display = "none";
-// }
-
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalNovoCurso) {
+    modalNovoCurso.style.display = "none";
   }
 }
